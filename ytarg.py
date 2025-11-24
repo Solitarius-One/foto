7d4dfbafa439c313e817fc7bd00b6d4ae6aaae63
Solitarius-One <anjirngab.coli@gmail.com>
2025-11-24 22:08:44 +0700

    Update ytarg.py

--- ytarg.py
+++ ytarg.py
@@ -1,8 +1,28 @@
+import argparse
 from yt_dlp import YoutubeDL
 
-keyword = "lagu lofi tidur"   # bisa diganti bebas
-search_query = f"ytsearch:{keyword}"
+# -------------------------
+# Argparse
+# -------------------------
+parser = argparse.ArgumentParser(description="Download MP3 from YouTube")
+parser.add_argument("--keyword", type=str, help="Keyword pencarian YouTube")
+parser.add_argument("--url", type=str, help="URL YouTube langsung")
 
+args = parser.parse_args()
+
+# -------------------------
+# Tentukan mode input
+# -------------------------
+if args.url:
+    target = args.url
+elif args.keyword:
+    target = f"ytsearch:{args.keyword}"
+else:
+    raise ValueError("Harus pakai --keyword atau --url")
+
+# -------------------------
+# Opsi download MP3
+# -------------------------
 opts = {
     "format": "bestaudio/best",
     "postprocessors": [{
@@ -13,5 +33,8 @@ opts = {
     "outtmpl": "%(title).200s.%(ext)s"
 }
 
+# -------------------------
+# Eksekusi yt-dlp
+# -------------------------
 with YoutubeDL(opts) as ydl:
-    ydl.download([search_query])
+    ydl.download([target])
